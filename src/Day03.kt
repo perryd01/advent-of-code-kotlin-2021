f5c4712933a2db6234cc3d3c25379a75ef6eaf97
Dániel Miklós <daniel.miklos0@gmail.com>
2021-12-03 15:12:50 +0100

    reformat day3

--- src/Day03.kt
+++ src/Day03.kt
@@ -2,29 +2,17 @@ fun main() {
     fun part1(input: List<String>): Int {
         var gammaRates = ""
         for (c in input[0].indices) {
-            var zeroBit = 0
-            for (v in input) {
-                if (v[c] == '0') {
-                    zeroBit++
-                }
-            }
-            if (zeroBit < input.size / 2) {
-                gammaRates += '1'
-            } else {
-                gammaRates += '0'
-            }
-
+            gammaRates += if (input.getNthBit(c).count { it == 0 } < input.size / 2) '1' else '0'
         }
-
         val epRates = gammaRates.map { Integer.parseInt(it.toString()) xor 1 }
         val gRates = gammaRates.map { Integer.parseInt(it.toString(), 2) }
         return epRates.toDecimal() * gRates.toDecimal()
     }
 
     fun part2(input: List<String>): Int {
-        val oxigengen = OxygenGenerator(input)
-        val co2 = CO2(input)
-        return oxigengen * co2
+        val o = calculate(input, TaskEnum.Oxygen)
+        val co2 = calculate(input, TaskEnum.CO2)
+        return o * co2
     }
 
     val testInput = readInput("Day03_test")
@@ -43,35 +31,32 @@ fun List<String>.getNthBit(i: Int): List<Int> {
     return this.map { Integer.parseInt(it[i].toString()) }
 }
 
-fun OxygenGenerator(i: List<String>): Int {
+fun calculate(i: List<String>, t: TaskEnum): Int {
+    val r: Int
+    val e: Int
+
+    if (t == TaskEnum.Oxygen) {
+        r = 0
+        e = 1
+    } else {
+        r = 1
+        e = 0
+    }
+
     var l = i
     var n = l.size
     var index = 0
     while (n > 1) {
         val domValue = if (l.getNthBit(index)
                 .count { it == 0 } > l.size / 2.0
-        ) 0 else 1
+        ) r else e
         l.filter { Integer.parseInt(it[index].toString()) == domValue }.also { l = it }
         n = l.size
         index++
-    }
-    if (l.size != 1) {
-        throw Error("no success")
     }
     return l[0].map { it.toString().toInt() }.toDecimal()
 }
 
-fun CO2(i: List<String>): Int {
-    var l = i
-    var n = l.size
-    var index = 0
-    while (n > 1) {
-        val domValue = if (l.getNthBit(index)
-                .count { it == 0 } > l.size / 2.0
-        ) 1 else 0
-        l.filter { Integer.parseInt(it[index].toString()) == domValue }.also { l = it }
-        n = l.size
-        index++
-    }
-    return l[0].map { it.toString().toInt() }.toDecimal()
+enum class TaskEnum {
+    Oxygen, CO2
 }
